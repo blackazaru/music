f834104a8a07f2f7d193f2d02fd750e728a9533c
Danik <danik.cyrkunov@gmail.com>
2014-11-16 13:22:15 +0300

    add search

--- main.js
+++ main.js
@@ -3,48 +3,6 @@
  */
 
 
-var arr_audio = [
-    "http://dl.zaycev.net/15033/682830/bullet_for_my_valentine_-_alone_(zaycev.net).mp3",
-    "http://dl.zaycev.net/86004/2061302/rise_against_-_saviour_(zaycev.net).mp3",
-    "http://dl.zaycev.net/86004/421474/rise_against_-_hero_of_war_(zaycev.net).mp3",
-    "http://dl.zaycev.net/44153/482346/flyleaf_-_beautiful_bride_(zaycev.net).mp3",
-    "http://dl.zaycev.net/44153/63397/flyleaf_-_cassie_(zaycev.net).mp3",
-    "http://dl.zaycev.net/44153/511170/flyleaf_-_missing_(zaycev.net).mp3",
-    "http://dl.zaycev.net/1021/793531/30_seconds_to_mars_-_the_kill_(zaycev.net).mp3",
-    "http://dl.zaycev.net/44153/1610431/flyleaf_-_supernatural_(zaycev.net).mp3",
-    "http://dl.zaycev.net/86004/933692/rise_against_-_satellite_(zaycev.net).mp3",
-    "http://dl.zaycev.net/1021/977320/30_seconds_to_mars_-_a_beautiful_lie_(zaycev.net).mp3",
-    "http://dl.zaycev.net/1021/2978485/30_seconds_to_mars_-_stay_(zaycev.net).mp3",
-    "http://cs9-8v4.vk.me/p2/a1abd9abf7a523.mp3"];
-
-var arr_name = [
-"Bullet For My Valentine - Alone",
-"Rise Against - Saviour",
-"Rise Against - Hero Of War",
-"Flyleaf - Beautiful Bride",
-"Flyleaf - Cassie",
-"Flyleaf - Missing",
-"30 Seconds To Mars - The Kill",
-"Flyleaf - Supernatural",
-"Rise Against - Satellite",
-"30 Seconds To Mars - A Beautiful Lie",
-"30 Seconds To Mars - Stay",
-"Nobody.one – Genesis"];
-
-var album = [
-    "http://cdn.licklibrary.com////cdn.licklibrary.com/~/media/LL/Uploads/news/F/Fever.ashx",
-    "http://www.covertbooking.com/images/uploads/photos/RiseAgainst_AlbumArtwork_AppealToReason_RGBHiRes.jpg",
-    "http://www.covertbooking.com/images/uploads/photos/RiseAgainst_AlbumArtwork_AppealToReason_RGBHiRes.jpg",
-    "http://theywillrockyou.com/wp-content/uploads/2009/12/flyleafcover.jpg",
-    "http://img2.wikia.nocookie.net/__cb20100528162336/heavenmusic/images/thumb/1/16/Flyleaf_album.JPG/600px-Flyleaf_album.JPG",
-    "http://theywillrockyou.com/wp-content/uploads/2009/12/flyleafcover.jpg",
-    "http://1.bp.blogspot.com/-4lw4yRu8ZKM/UhO-J8zr7cI/AAAAAAAABQg/Z1869A96QcA/s1600/30_Seconds_To_Mars-A_Beautiful_Lie-Frontal.jpg",
-    "http://theywillrockyou.com/wp-content/uploads/2009/12/flyleafcover.jpg",
-    "http://cs618618.vk.me/v618618272/1d9c8/BCChxBsAnXs.jpg",
-    "http://1.bp.blogspot.com/-4lw4yRu8ZKM/UhO-J8zr7cI/AAAAAAAABQg/Z1869A96QcA/s1600/30_Seconds_To_Mars-A_Beautiful_Lie-Frontal.jpg",
-    "http://www.album-info.ru/Shared/Cover/2014/30-seconds-to-mars-stay-song.jpg",
-    "http://cs625521.vk.me/v625521603/9516/oI8V_tuOt5E.jpg"
-]
 
 
 
@@ -76,6 +34,7 @@ $(function(){
 });
 
 window.onload = function(){
+     arr_name = sortBubble(arr_name);
     $("#list").click(function(){
         if (document.getElementById("list_block").getAttribute("open") == "false") {
             document.getElementById("list_block").setAttribute("open", "true");
@@ -86,6 +45,22 @@ window.onload = function(){
             $("#list_block").animate({
                 height: "90%"
             }, 700);
+            input = document.createElement('input');
+            input.typeName = "text";
+            input.id = "input";
+            document.getElementById("list_block").appendChild(input);
+
+            $("#input").keyup(function(e) {
+                if(e.keyCode == 13)  {
+                    serch($(this).val());
+                }
+            });
+
+            for(var i=0; i<arr_name.length; i++){
+                if (document.getElementById("song_"+i) !== null)
+                    document.getElementById("song_"+i).remove();
+            }
+
             for(var i=0; i<arr_name.length; i++){
                 var iDiv = document.createElement('div');
                 iDiv.id = 'song_'+i;
@@ -102,9 +77,10 @@ window.onload = function(){
                 height: "0px"
             }, 700);
             setTimeout(function(){
-            for(var i=0; i<arr_name.length; i++){
-                    document.getElementById("song_"+i).remove();
-            }
+                document.getElementById("input").remove();
+                for(var i=0; i<arr_name.length; i++){
+                       document.getElementById("song_"+i).remove();
+                }
             },690);
         }
     });
@@ -151,6 +127,71 @@ function next(audio, index){
     audio.play();
 
 }
+
+function serch(query){
+    if (query !== ""){
+        for(var i=0; i<arr_name.length; i++){
+            if (document.getElementById("song_"+i) !== null)
+                document.getElementById("song_"+i).remove();
+        }
+
+        for(var i = 0; i<arr_name.length; i++){
+            if (arr_name[i].toUpperCase().indexOf(query.toUpperCase()) !== -1) {
+                var iDiv = document.createElement('div');
+                iDiv.id = 'song_' + i;
+                iDiv.className = 'song_' + i;
+                iDiv.innerHTML = arr_name[i];
+                iDiv.style.borderBottom = "1px solid";
+                iDiv.style.cursor = "pointer";
+                iDiv.setAttribute("onclick", "next(a[0]," + i + ")");
+                document.getElementById("list_block").appendChild(iDiv);
+            }
+        }
+    }else{
+        for(var i=0; i<arr_name.length; i++){
+            if (document.getElementById("song_"+i) !== null)
+                document.getElementById("song_"+i).remove();
+        }
+        for(var i = 0; i<arr_name.length; i++){
+                var iDiv = document.createElement('div');
+                iDiv.id = 'song_' + i;
+                iDiv.className = 'song_' + i;
+                iDiv.innerHTML = arr_name[i];
+                iDiv.style.borderBottom = "1px solid";
+                iDiv.style.cursor = "pointer";
+                iDiv.setAttribute("onclick", "next(a[0]," + i + ")");
+                document.getElementById("list_block").appendChild(iDiv);
+        }
+    }
+
+}
+
+function sortBubble(data) {
+    var tmp;
+
+    for (var i = data.length - 1; i > 0; i--) {
+        for (var j = 0; j < i; j++) {
+            if (data[j] > data[j+1]) {
+                tmp = data[j];
+                data[j] = data[j+1];
+                data[j+1] = tmp;
+                var  c = arr_audio[j];arr_audio[j] = arr_audio[j+1];arr_audio[j+1] = c;
+                c = album[j];album[j] = album[j+1];album[j+1] = c;
+            }
+        }
+    }
+    return data;
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ music.js
@@ -0,0 +1,54 @@
+/**
+ * Created by Danik Tsyrkunov on 16.11.2014.
+ */
+var arr_audio = [
+    "http://dl.zaycev.net/15033/682830/bullet_for_my_valentine_-_alone_(zaycev.net).mp3",
+    "http://dl.zaycev.net/86004/2061302/rise_against_-_saviour_(zaycev.net).mp3",
+    "http://dl.zaycev.net/86004/421474/rise_against_-_hero_of_war_(zaycev.net).mp3",
+    "http://dl.zaycev.net/44153/482346/flyleaf_-_beautiful_bride_(zaycev.net).mp3",
+    "http://dl.zaycev.net/44153/63397/flyleaf_-_cassie_(zaycev.net).mp3",
+    "http://dl.zaycev.net/44153/511170/flyleaf_-_missing_(zaycev.net).mp3",
+    "http://dl.zaycev.net/1021/793531/30_seconds_to_mars_-_the_kill_(zaycev.net).mp3",
+    "http://dl.zaycev.net/44153/1610431/flyleaf_-_supernatural_(zaycev.net).mp3",
+    "http://dl.zaycev.net/86004/933692/rise_against_-_satellite_(zaycev.net).mp3",
+    "http://dl.zaycev.net/1021/977320/30_seconds_to_mars_-_a_beautiful_lie_(zaycev.net).mp3",
+    "http://dl.zaycev.net/1021/2978485/30_seconds_to_mars_-_stay_(zaycev.net).mp3",
+    "http://cs9-8v4.vk.me/p2/a1abd9abf7a523.mp3",
+    "http://dl.zaycev.net/97258/3074526/system_of_a_down_-_b.y.o.b._(zaycev.net).mp3",
+"http://dl.zaycev.net/139764/1615746/soad_-_revenga_(zaycev.net).mp3",
+"http://dl.zaycev.net/139764/1266916/soad_-_radio_video_(zaycev.net).mp3"];
+
+var arr_name = [
+    "Bullet For My Valentine - Alone",
+    "Rise Against - Saviour",
+    "Rise Against - Hero Of War",
+    "Flyleaf - Beautiful Bride",
+    "Flyleaf - Cassie",
+    "Flyleaf - Missing",
+    "30 Seconds To Mars - The Kill",
+    "Flyleaf - Supernatural",
+    "Rise Against - Satellite",
+    "30 Seconds To Mars - A Beautiful Lie",
+    "30 Seconds To Mars - Stay",
+    "Nobody.one – Genesis",
+    "SOAD - B.Y.O.B.",
+"SOAD - Revenga",
+"SOAD - Radio/Video"];
+
+var album = [
+    "http://cdn.licklibrary.com////cdn.licklibrary.com/~/media/LL/Uploads/news/F/Fever.ashx",
+    "http://www.covertbooking.com/images/uploads/photos/RiseAgainst_AlbumArtwork_AppealToReason_RGBHiRes.jpg",
+    "http://www.covertbooking.com/images/uploads/photos/RiseAgainst_AlbumArtwork_AppealToReason_RGBHiRes.jpg",
+    "http://theywillrockyou.com/wp-content/uploads/2009/12/flyleafcover.jpg",
+    "http://img2.wikia.nocookie.net/__cb20100528162336/heavenmusic/images/thumb/1/16/Flyleaf_album.JPG/600px-Flyleaf_album.JPG",
+    "http://theywillrockyou.com/wp-content/uploads/2009/12/flyleafcover.jpg",
+    "http://1.bp.blogspot.com/-4lw4yRu8ZKM/UhO-J8zr7cI/AAAAAAAABQg/Z1869A96QcA/s1600/30_Seconds_To_Mars-A_Beautiful_Lie-Frontal.jpg",
+    "http://theywillrockyou.com/wp-content/uploads/2009/12/flyleafcover.jpg",
+    "http://cs618618.vk.me/v618618272/1d9c8/BCChxBsAnXs.jpg",
+    "http://1.bp.blogspot.com/-4lw4yRu8ZKM/UhO-J8zr7cI/AAAAAAAABQg/Z1869A96QcA/s1600/30_Seconds_To_Mars-A_Beautiful_Lie-Frontal.jpg",
+    "http://www.album-info.ru/Shared/Cover/2014/30-seconds-to-mars-stay-song.jpg",
+    "http://cs625521.vk.me/v625521603/9516/oI8V_tuOt5E.jpg",
+    "http://cdn.albumoftheyear.org/album/mezmerize.jpg",
+    "http://cdn.albumoftheyear.org/album/mezmerize.jpg",
+    "http://cdn.albumoftheyear.org/album/mezmerize.jpg"
+]
